c333aeac4f5d92c11d444ea27de25cf806475753
Pra3t0r5 <fernando.albertengo@gmail.com>
2019-03-14 20:58:08 -0300

    ejercicio 4 completado

--- ejercicio-4.py
+++ ejercicio-4.py
@@ -11,11 +11,13 @@ vocales = ["a","e","i","o","u","A","E","I","O","U"]
 print("ingrese una letra")
 letra = input()
 
-def esVocal(str letra):
-    if letra in vocales 
+def esVocal(letra):
+    if letra in vocales:
         return True
-    return False
+    else:
+        return False
 
-if esVocal(letra)
-    print({letra}" es vocal")
-print({letra}" es consonante")
+if esVocal(letra):
+    print({letra}," es vocal")
+else:
+    print({letra}," es consonante")
