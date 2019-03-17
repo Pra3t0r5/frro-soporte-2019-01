d4fa3f727ea044e7187c803fd356225335803f37
Pra3t0r5 <fernando.albertengo@gmail.com>
2019-03-16 22:32:03 -0300

    testing ide fuctionality

--- ejercicio-1.py
+++ ejercicio-1.py
@@ -1,12 +1,15 @@
 import math
-a=0
-b=1
 
-def mayor(a,b):
-    if a >= b
+a = 0
+b = 1
+
+
+def mayor(a, b):
+    if a >= b:
         return a
     return b
 
-print(mayor(a,b))
 
-assert mayor(3,2) == 3
+print(mayor(a, b))
+
+assert mayor(3, 2) == 3
