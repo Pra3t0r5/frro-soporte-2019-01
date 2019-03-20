97cb1e2ef6cff8ba860b4db2d95721598cd66bfa
Pra3t0r5 <fernando.albertengo@gmail.com>
2019-03-19 21:23:31 -0300

    completada guia practica 1

--- ejercicio-3.py
+++
@@ -1,8 +0,0 @@
-cadena = "hola esto es soporte"
-largo = len(cadena)
-print("longitud de la cadena", largo)
-
-array = ["matias", "lucas", "fernando",5 ,1 ,4]
-largo = len(array)
-print("numero de elementos en arreglo: ", largo)
-
