523157161e0999589a50eb7d03da6bc81ad8b424
Pra3t0r5 <fernando.albertengo@gmail.com>
2019-03-23 22:24:35 -0300

    inicializacion practico-02

--- ejercicio-06.py
+++
@@ -1,8 +0,0 @@
-#Definir una función inversa() que calcule la inversión de una cadena
-
-def inversa(cadena):
-    cadenaInvertida = cadena[::-1]
-    return cadenaInvertida
-
-assert(inversa("estoy probando") == "odnaborp yotse")
-
