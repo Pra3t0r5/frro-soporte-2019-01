4e39bb6a13a4c765b4e0bfd83002ea2814ae01d8
Matías <mulassimatias@gmail.com>
2019-03-25 09:54:21 -0300

    solucionando conflictos

---
+++ ejercicio01.py
@@ -0,0 +1,22 @@
+# Implementar la clase Rectangulo que contiene una base y una altura, y el método area.
+
+class Rectangulo:
+
+    def __init__(self, base, altura):
+        self.base= base
+        self.altura=altura
+
+
+    def area(self):
+        return self.base*self.altura
+
+
+class Circulo:
+    def __init__(self, radio):
+        self.radio=radio
+
+    def area(self):
+
+
+    def perimetro(self):
+        pass
