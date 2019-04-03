4f1c783d6aabddc6abbba642daef13d563761673
Pra3t0r5 <fernando.albertengo@gmail.com>
2019-04-03 18:06:54 -0300

    agregados ejemplos 3 de abril

---
+++ ejemplos_04-03.py
@@ -0,0 +1,121 @@
+cuadrados = [x**2 for x in range(10)]
+
+cuad_imp = [x**2 for x in range(10) if x % 2 == 1]
+cuad_par = [x**2 for x in range(10) if x % 2 == 0]
+
+print("todos", cuadrados)
+
+# Clases
+
+x = MiClase()  # se sobreentiende como clase porque inicia en mayuscula, por lo que no es funcion (por convencion)
+
+
+def __init__(self):  # por convencion se define al inicio y las funciones llamadas dentro no necesitan ser definidas ANTES como en funcones normales
+    self.datos = []
+
+
+# llamada
+# la instanciacion de una clase lo llama automaticamente (constructor)
+__init__(self)
+
+
+class Cuadrado:
+    def __init__(self):
+        self.lados = 4
+
+
+ocuadrado = Cuadrado()
+print(ocuadrado.lados)
+
+# Clases--------------------------------------
+
+
+class Auto:
+    def __init__(self, color):
+        self.color = color
+
+# Herencia--------------------------------------
+
+
+class Figura:
+    nlados = 0
+
+    def muestra(self):
+        print(nlados)
+
+
+class Cuadrado(Figura):
+    def __init__(self):
+        self.nlados = 4
+
+    def muestra(self):
+        Figura.muestra(self)
+        print('cuadrado tiene otras cualidades')
+
+# Metodos de Formato--------------------------------------
+# El fuerte de py esta en todo lo que corre en el servidor. La interfaz nativa de python server es pobre, solo muestra textos en consola, excepto que se le anadan librerias
+
+#Conversion a mayuscula de la primer letra
+cadena = 'hola clase'
+cadena.capitalize() #Hola clase
+cadena.upper()
+cadena.lower()
+print(cadena.center(40,'=')) #centra la cadena en medio de un string de 40 iguales
+
+# Metodos de Busqueda--------------------------------------
+
+#cantidad de veces que aparece
+coount(subcadena[,inicio,fin])
+cadena = 'hola clase'
+cadena.count('a')
+#buscar en una subcadena
+
+# Metodos de Validacion--------------------------------------
+cadena.startswith('ho') #true
+cadena.startswith('ho',3) #false
+cadena.endswith(texto[ini,fin])
+cadena.isalum()
+#hay mas
+
+# Metodos de Sustitucion--------------------------------------
+format(*args,**kwargs) #**kwargs son estructuras cadena valor
+
+#reemplazo de posicion, la cadena no cambia, y le pasas varios datos. 0 y 1 son placeholders
+cadena = 'hola {0} que tengas un {1}'
+cadena.format('Juan','buen dia')
+
+#reemplazo por palabras clave
+cadena = 'hola {nombre} que tengas un {saludo}'
+cadena.format(saludo='buen dia',nombre='Juan') #deja de importar el orden
+
+#reemplazo de texto en una cadena
+#sintaxis replace.(txt a buscar, txt reemplazo)
+cadena = 'hola nombre'
+cadena.replace('nombre', 'joaquin') #las cadenas son inmutables, devuelve una nueva
+
+#eliminar caracteres (x def: espacios)
+strip([caracter]) #de derecha a izquierda
+#hay variantes
+
+# Metodos de Union--------------------------------------
+sep = '-'
+sep.join(('a','b','c')) #a-b-c
+
+formato_nro_fac = ("N 0000-0","-0000(ID:",")")
+nro=275......................
+
+# Metodos de Division--------------------------------------
+#partition(separador)
+tupla = "https://www.google.com".partition("www.")
+print(tupla) #entrega lo que esta antes, el separador y lo que esta despues entre comas simples y separadas por comas
+print("Protocolo:{0}\nDominio{1}".format(protocolo,dominio))
+#Protocolo: http://
+#Dominio: google.com
+
+#split(separador)
+cadena = "libro, cuaderno, lapiz"
+lista = cadena.split(',') #entrega [libro, cuaderno, lapiz]
+
+
+
+
--- practico_02/ejercicio_03.py
+++ practico_02/ejercicio_03.py
@@ -9,27 +9,44 @@
 # - es_mayor_edad(): indica si es mayor de edad, devuelve un booleano.
 # - print_data(): imprime por pantalla toda la información del objeto.
 # - generar_dni(): genera un número aleatorio de 8 cifras y lo guarda dentro del atributo dni.
+import random
 
 
 class Persona:
-    sexosValidos = {"H","M"}
-    
-    def __init__(self, nombre, edad, sexo, peso, altura):
+    sexosValidos = {"H", "M"}
+
+    def __init__(self, nombre, edad, sex, peso, altura):
         self.nombre = nombre
         self.edad = edad
-        if sexo in self.sexosValidos: 
-            self.sexo 
-        else:
-            raise ValueError("results: debe ser uno de %r." % self.sexosValidos)
+        self.sexo = self.definir_sex(sex)
         self.peso = peso
         self.altura = altura
+        self.dni = self.generar_dni()
+
+    def definir_sex(self, sex):
+        while(sex not in self.sexosValidos):
+            if sex in self.sexosValidos:
+                return sex
+            else:
+                raise ValueError("results: debe ser uno de %r." %
+                                 self.sexosValidos)
 
     def es_mayor_edad(self):
-        pass
+        if(self.edad > 17):
+            return True
+        else:
+            return False
 
     # llamarlo desde __init__
     def generar_dni(self):
-        pass
+        return random.randint(1, 99999999)
 
     def print_data(self):
-        pass
+        print("Nombre: ", self.nombre, "\nDNI: ", self.dni, "\nEdad: ", self.edad,
+              "\nSexo: ", self.sexo, "\nPeso: ", self.peso, "kg\nAltura: ", self.altura, "m")
+
+
+per = Persona("Pepito Manolo", 25, "H", 87.6, 1.70)
+per.print_data()
+#assert(per.sexo) == "H"
+assert(per.es_mayor_edad()) == True
