82ed6c3c06dcb31482a63c8bf47ea671e77e21e0
Lucas Pavan <lucaspavan.lp@gmail.com>
2019-04-24 20:24:48 -0300

    practico 03 limpio

--- practico_01/ejercicio-02.py
+++
@@ -1,8 +0,0 @@
-# Implementar la función mayor, que reciba tres números y devuelva el mayor de ellos.
-
-def max_de_tres(a,b,c):
-    n=max(a,b,c)
-    return n
-
-assert max_de_tres(10, 5, 3) == 10
-assert max_de_tres(9, 18, 15) == 18
