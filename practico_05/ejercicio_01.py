f26dca2dd5a4fc1e6853a9b7671002ba001ea319
Matías <mulassimatias@gmail.com>
2019-05-16 12:02:06 -0300

    Finish TP5 with 1 error

---
+++ practico_05/ejercicio_01.py
@@ -0,0 +1,18 @@
+# Implementar un modelo Socio a traves de Alchemy que cuente con los siguientes campos:
+# - id_socio: entero (clave primaria, auto-incremental, unico)
+# - dni: entero (unico)
+# - nombre: string (longitud 250)
+# - apellido: string (longitud 250)
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+
+Base = declarative_base()
+
+
+class Socio(Base):
+    __tablename__ = 'socios'
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
+    dni = Column(Integer, unique=True)
+    nombre = Column(String(250))
+    apellido = Column(String(250))
