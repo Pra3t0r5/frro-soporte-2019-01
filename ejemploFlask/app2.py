541b75f7b9a66ad309c989cc7110844dcb8e06bd
Matías <mulassimatias@gmail.com>
2019-09-24 15:24:45 -0300

    agregado de register.html

--- ejemploFlask/app2.py
+++ ejemploFlask/app2.py
@@ -1,26 +1,39 @@
-from flask import Flask,render_template
-from frro_soporte_2019_01.ejemploFlask.data import Articles
+from flask import Flask,render_template, flash, redirect , url_for , session ,request,logging
+from flaskext.mysql import MySQL
+from frro_soporte_2019_01.ejemploFlask.data.data import Articles
+from wtforms import Form, StringField , TextAreaField ,PasswordField , validators
+from passlib.hash import sha256_crypt
 
-app= Flask(__name__)
-app.debug = True #Actuliza automaticamente la pagina ante un cambio.
+app = Flask(__name__)
+app.debug = True
+mysql = MySQL(app)
 Articles = Articles()
-@app.route('/') #Aca se define la direccion de ruta de la pagina.
-
+@app.route('/')
 def index():
-    return render_template('home.html') #redireccion a una pagina html
-
-@app.route('/about') #Aca se define la direccion de ruta a la pagina about.html.
+    return render_template('home.html')
+@app.route('/about')
 def about():
     return render_template('about.html')
-
 @app.route('/articles')
 def articles():
     return render_template('articles.html',articles = Articles)
-
 @app.route('/article/<string:id>/')
 def article(id):
     return render_template('article.html',id=id)
+class RegisterForm(Form):
+    name = StringField('Name',[validators.Length(min=1,max=50)])
+    username = StringField('Username',[validators.Length(min=4,max=25)])
+    email = StringField('Email',[validators.Length(min=4,max=25)])
+    password = PasswordField('Password', [ validators.DataRequired
+    (),validators.EqualTo('confirm',message ='passwords do not match')])
+    confirm = PasswordField('Confirm password')
 
-if __name__ == '__main__':
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return render_template('register.html',form=form)
+
+
+if __name__ =='__main__':
     app.run()
-
