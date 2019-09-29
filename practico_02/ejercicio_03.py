3118430e5a11bc5ecbc0e997436cd7fc1742474b
Pra3t0r5 <fernando.albertengo@gmail.com>
2019-09-28 21:35:59 -0300

    merge

--- practico_02/ejercicio_03.py
+++ practico_02/ejercicio_03.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-# Implementar la clase Persona que cumpla las siguientes condiciones:
-=======
 # 3)Implementar la clase Persona que cumpla las siguientes condiciones:
->>>>>>> 128b4c1881e401039672e9a890828153d18666bd
 # Atributos:
 # - nombre.
 # - edad.
@@ -15,32 +11,8 @@
 # - generar_dni(): genera un número aleatorio de 8 cifras y lo guarda dentro del atributo dni.
 import random
 
-<<<<<<< HEAD
-
 class Persona:
-    sexosValidos = {"H", "M"}
-
-    def __init__(self, nombre, edad, sex, peso, altura):
-        self.nombre = nombre
-        self.edad = edad
-        self.sexo = self.definir_sex(sex)
-        self.peso = peso
-        self.altura = altura
-        self.dni = self.generar_dni()
-
-    def definir_sex(self, sex):
-        while(sex not in self.sexosValidos):
-            if sex in self.sexosValidos:
-                return sex
-            else:
-                raise ValueError("results: debe ser uno de %r." %
-                                 self.sexosValidos)
-
-    def es_mayor_edad(self):
-        if(self.edad > 17):
-=======
-class Persona:
-
+    
     def __init__(self, nombre, edad, sexo, peso, altura):
         self.nombre = nombre
         self.edad = int(edad)
@@ -51,26 +23,10 @@ class Persona:
 
     def es_mayor_edad(self):
         if self.edad >= 18:
->>>>>>> 128b4c1881e401039672e9a890828153d18666bd
             return True
         else:
             return False
 
-<<<<<<< HEAD
-    # llamarlo desde __init__
-    def generar_dni(self):
-        return random.randint(1, 99999999)
-
-    def print_data(self):
-        print("Nombre: ", self.nombre, "\nDNI: ", self.dni, "\nEdad: ", self.edad,
-              "\nSexo: ", self.sexo, "\nPeso: ", self.peso, "kg\nAltura: ", self.altura, "m")
-
-
-per = Persona("Pepito Manolo", 25, "H", 87.6, 1.70)
-per.print_data()
-#assert(per.sexo) == "H"
-assert(per.es_mayor_edad()) == True
-=======
    # llamarlo desde __init__
     def generar_dni(self):
         dni=random.randrange(100000000)
@@ -84,4 +40,3 @@ assert(per.es_mayor_edad()) == True
         print("Peso: ", self.peso)
         print("Altura: ", self.altura)
         print("Dni: ", self.dni)
->>>>>>> 128b4c1881e401039672e9a890828153d18666bd
