725c5f7f3d6142d449c288e64a08a040baa0abe6
Pra3t0r5 <fernando.albertengo@gmail.com>
2019-10-08 20:28:26 -0300

    agregado auth y main iniciales

--- cerveceria/cerveweb/main.py
+++ cerveceria/cerveweb/main.py
@@ -4,12 +4,10 @@ from . import db
 
 main = Blueprint('main', __name__)
 
-
 @main.route('/')
 def index():
     return render_template('index.html')
 
-
 @main.route('/profile')
 def profile():
-    return 'Profile'
+    return render_template('profile.html')
