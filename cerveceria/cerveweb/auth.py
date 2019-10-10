5aa8d642a35b1fc25c33d5cae9ea22ed699f60ad
Pra3t0r5 <fernando.albertengo@gmail.com>
2019-10-10 19:55:58 -0300

    agregando funcionalidad registro

--- cerveceria/cerveweb/auth.py
+++ cerveceria/cerveweb/auth.py
@@ -3,7 +3,6 @@ from . import db
 
 auth = Blueprint('auth', __name__)
 
-
 @auth.route('/login')
 def login():
     return render_template('login.html')
@@ -12,6 +11,10 @@ def login():
 def contacto():
     return render_template('contact.html')
 
-@auth.route('/registro')
-def registro():
-    return render_template('register.html')
+@auth.route("/registro", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm() #create an instance of RegistrationForm Class. we have given our object a name form
+    if form.validate_on_submit(): # check if all form fields are valid
+        flash(f'Account created for {form.username.data}!', 'success') # show an alert message to the user on screen
+        return redirect(url_for('home')) # Redirect the user back to the home after successful login
+    return render_template('register.html', title='Register', form=form)
---
+++ cerveceria/cerveweb/models.py
@@ -0,0 +1,25 @@
+from . import db
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+admin = User('admin', 'admin@example.com')
+
+db.create_all() # In case user table doesn't exists already. Else remove it.    
+
+db.session.add(admin)
+
+db.session.commit() # This is needed to write the changes to database
+
+User.query.all()
+
+User.query.filter_by(username='admin').first()
