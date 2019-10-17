7b38eefae0f81c80d71125c7aa417e662cf97808
Pra3t0r5 <fernando.albertengo@gmail.com>
2019-10-17 20:48:31 -0300

    agregado flask-login al installer y mejorado mario

--- cerveceria/cerveweb/install.sh
+++ cerveceria/cerveweb/install.sh
@@ -6,7 +6,7 @@ else (
         apt install python3
         apt install python3-pip
         source ../bin/activate
-        pip3 install flask flask-sqlalchemy sqlalchemy wtforms flask-wtf mysql-connector-python
+        pip3 install flask flask-sqlalchemy sqlalchemy wtforms flask-wtf mysql-connector-python flask-login
         mysql -e "create user 'cerveweb'@'localhost' identified by 'beerjesus'; grant all privileges on *.* to 'cerveweb'@'localhost'; flush privileges; create database cerveweb;"
         wget https://trello-attachments.s3.amazonaws.com/5d952a113b1ad84a91fb61a1/5d952f57131cda03c5780e94/90e189b4b088da4f16c02171412836a3/cerveweb_dump_20191005.sql
         mysql -ucerveweb -pbeerjesus cerveweb < cerveweb_dump_20191005.sql
