678ce2bcab2720b9e3cd5a45d22dfc7671ebc4f4
Pra3t0r5 <fernando.albertengo@gmail.com>
2019-10-25 09:06:10 -0300

    install

--- cerveceria/install.sh
+++ cerveceria/install.sh
@@ -6,7 +6,7 @@ else (
         apt install python3
         apt install python3-pip
         source bin/activate
-        pip3 install flask flask-sqlalchemy sqlalchemy wtforms flask-wtf mysql-connector-python flask-login
+        pip3 install flask flask-sqlalchemy sqlalchemy wtforms flask-wtf mysql-connector-python flask-login flask-admin
         mysql -e "create user 'cerveweb'@'localhost' identified by 'beerjesus'; grant all privileges on *.* to 'cerveweb'@'localhost'; flush privileges; drop database if exists cerveweb; create database cerveweb;"
         wget https://trello-attachments.s3.amazonaws.com/5d952a113b1ad84a91fb61a1/5d952f57131cda03c5780e94/ad4f57509c4a8ee9a5f225c5d8683ae0/cerveweb.sql
         # wget https://trello-attachments.s3.amazonaws.com/5d952a113b1ad84a91fb61a1/5d952f57131cda03c5780e94/077abeb91cd4bb2c1f997baa5d0680e2/cerveweb_dump_20191018.sql
